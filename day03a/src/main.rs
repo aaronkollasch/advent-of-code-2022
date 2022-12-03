e378929582126e63a7b00aa16a46183c5752cdfb
Aaron Kollasch <aaron@kollasch.dev>
2022-12-03 14:21:14 -0500

    Add day 3

---
+++ day03a/src/main.rs
@@ -0,0 +1,29 @@
+// use std::collections::HashSet;
+
+pub fn main() {
+    println!(
+        "{}",
+        include_bytes!("../input.txt")
+            .split(|b| *b == b'\n')
+            .filter(|l| l.len() > 0)
+            .map(|l| l.split_at(l.len() / 2))
+            .map(|(a, b)| {
+                // let mut a_set: HashSet<u8> = HashSet::with_capacity(a.len());
+                // for i in 0..a.len() {
+                //     a_set.insert(a[i]);
+                // };
+                b.iter()
+                    .filter(|b| a.contains(b))
+                    .map(|b| {
+                        if *b >= b'a' {
+                            (b - b'a') as u16 + 1
+                        } else {
+                            (b - b'A') as u16 + 27
+                        }
+                    })
+                    .next()
+                    .unwrap()
+            })
+            .sum::<u16>()
+    );
+}
---
+++ day03b/src/main.rs
@@ -0,0 +1,22 @@
+pub fn main() {
+    println!(
+        "{}",
+        include_bytes!("../input.txt")
+            .split(|b| *b == b'\n')
+            .filter(|l| l.len() > 0)
+            .collect::<Vec<_>>()
+            .chunks(3)
+            .map(|group| group[0]
+                .iter()
+                .find(|item| group[1].contains(item) && group[2].contains(item))
+                .unwrap())
+            .map(|b| {
+                if *b >= b'a' {
+                    (b - b'a') as u16 + 1
+                } else {
+                    (b - b'A') as u16 + 27
+                }
+            })
+            .sum::<u16>()
+    );
+}
