4abee05b822e726b9323b5ddbc3673d004c79a50
Aaron Kollasch <aaron@kollasch.dev>
2022-12-04 01:47:13 -0500

    Add day 4

---
+++ day04b/src/main.rs
@@ -0,0 +1,13 @@
+pub fn main() {
+    println!(
+        "{}",
+        include_str!("../target.txt")
+            .lines()
+            .map(|l| l
+                .split(&[',', '-'][..])
+                .filter_map(|s| s.parse::<u16>().ok())
+                .collect::<Vec<u16>>())
+            .filter(|v| v.len() == 4 && v[1] >= v[2] && v[0] <= v[3])
+            .count()
+    );
+}
