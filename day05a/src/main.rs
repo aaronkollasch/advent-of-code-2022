4d019659feb1b837c39044a27f4bb99c4ac9218d
Aaron Kollasch <aaron@kollasch.dev>
2022-12-05 11:49:39 -0500

    Speedup day 5, 800 µs -> 60 µs total

--- day05a/src/main.rs
+++ day05a/src/main.rs
@@ -1,17 +1,19 @@
+use itertools::Itertools;
+
 pub fn main() {
-    let (stacks, instructions) = include_str!("../target.txt").split_once("\n\n").unwrap();
+    let d = include_bytes!("../input.txt");
+    let (stacks, instructions) = d.split_at(d.windows(2).position(|b| b == b"\n\n").unwrap() + 2);
 
     // load initial stack state
-    let mut stack_lines = stacks.lines().rev();
-    let stack_indices = stack_lines.next().unwrap();
-    let num_stacks = stack_indices.len() / 4 + 1;
+    let mut stack_lines = stacks.split(|b| *b == b'\n').rev().skip(2);
+    let num_stacks = stack_lines.next().unwrap().len() / 4 + 1;
     let mut stacks: Vec<Vec<u8>> = Vec::with_capacity(num_stacks);
     for _ in 0..num_stacks {
         stacks.push(Vec::<u8>::new());
     }
     stack_lines.for_each(|l| {
         for i in 0..num_stacks {
-            let c = l.as_bytes()[1 + i * 4];
+            let c = l[1 + i * 4];
             if c != b' ' {
                 stacks[i].push(c);
             }
@@ -19,33 +21,22 @@ pub fn main() {
     });
 
     // execute instructions
-    instructions.lines().enumerate().for_each(|(i_line, l)| {
-        let mut l_words = l.split(' ');
-        let n = l_words.nth(1).unwrap().parse::<usize>().unwrap();
-        let from = l_words.nth(1).unwrap().parse::<usize>().unwrap() - 1;
-        let to = l_words.nth(1).unwrap().parse::<usize>().unwrap() - 1;
-        for i in 0..n {
-            let c = stacks
-                .get_mut(from)
-                .expect(format!("{}, {}: get stack {}: {}", i_line, i, from, num_stacks).as_str())
-                .pop()
-                .expect(
-                    format!(
-                        "{}, {}: pop {} from {}: {}",
-                        i_line,
-                        i,
-                        n,
-                        from,
-                        stacks[from].len()
-                    )
-                    .as_str(),
-                );
-            stacks
-                .get_mut(to)
-                .expect(format!("{}, {}: get stack {}: {}", i_line, i, to, num_stacks).as_str())
-                .push(c);
-        }
-    });
+    instructions
+        .split(|b| *b == b'\n')
+        .filter(|l| l.len() > 0)
+        .for_each(|l| {
+            let (n, from, to): (usize, _, _) = l
+                .split(|b| b == &b' ')
+                .skip(1)
+                .step_by(2)
+                .map(|n| atoi::atoi(n).unwrap())
+                .collect_tuple()
+                .unwrap();
+            for _i in 0..n {
+                let c = stacks.get_mut(from-1).unwrap().pop().unwrap();
+                stacks.get_mut(to-1).unwrap().push(c);
+            }
+        });
 
     // print last stacks
     println!(
--- day05b/src/main.rs
+++ day05b/src/main.rs
@@ -1,19 +1,20 @@
+use itertools::Itertools;
 use std::collections::LinkedList;
 
 pub fn main() {
-    let (stacks, instructions) = include_str!("../target.txt").split_once("\n\n").unwrap();
+    let d = include_bytes!("../input.txt");
+    let (stacks, instructions) = d.split_at(d.windows(2).position(|b| b == b"\n\n").unwrap() + 2);
 
     // load initial stack state
-    let mut stack_lines = stacks.lines().rev();
-    let stack_indices = stack_lines.next().unwrap();
-    let num_stacks = stack_indices.len() / 4 + 1;
+    let mut stack_lines = stacks.split(|b| *b == b'\n').rev().skip(2);
+    let num_stacks = stack_lines.next().unwrap().len() / 4 + 1;
     let mut stacks: Vec<LinkedList<u8>> = Vec::with_capacity(num_stacks);
     for _ in 0..num_stacks {
         stacks.push(LinkedList::<u8>::new());
     }
     stack_lines.for_each(|l| {
         for i in 0..num_stacks {
-            let c = l.as_bytes()[1 + i * 4];
+            let c = l[1 + i * 4];
             if c != b' ' {
                 stacks[i].push_back(c);
             }
@@ -21,21 +22,21 @@ pub fn main() {
     });
 
     // execute instructions
-    instructions.lines().enumerate().for_each(|(i_line, l)| {
-        let mut l_words = l.split(' ');
-        let n = l_words.nth(1).unwrap().parse::<usize>().unwrap();
-        let from = l_words.nth(1).unwrap().parse::<usize>().unwrap() - 1;
-        let to = l_words.nth(1).unwrap().parse::<usize>().unwrap() - 1;
-        let split_index = stacks[from].len() - n;
-        let mut stack_part = stacks
-            .get_mut(from)
-            .expect(format!("{}: get stack {}: {}", i_line, from, num_stacks).as_str())
-            .split_off(split_index);
-        stacks
-            .get_mut(to)
-            .expect(format!("{}: get stack {}: {}", i_line, to, num_stacks).as_str())
-            .append(&mut stack_part);
-    });
+    instructions
+        .split(|b| *b == b'\n')
+        .filter(|l| l.len() > 0)
+        .for_each(|l| {
+            let (n, from, to): (usize, _, _) = l
+                .split(|b| b == &b' ')
+                .skip(1)
+                .step_by(2)
+                .map(|n| atoi::atoi(n).unwrap())
+                .collect_tuple()
+                .unwrap();
+            let split_index = stacks[from - 1].len() - n;
+            let mut stack_part = stacks.get_mut(from - 1).unwrap().split_off(split_index);
+            stacks.get_mut(to - 1).unwrap().append(&mut stack_part);
+        });
 
     // print last stacks
     println!(
