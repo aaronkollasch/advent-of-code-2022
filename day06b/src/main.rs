13e0e4ac09461bd382c14565d12b21cea8b395f9
Aaron Kollasch <aaron@kollasch.dev>
2022-12-06 02:36:18 -0500

    Add solution for day 6

--- day06b/src/main.rs
+++ day06b/src/main.rs
@@ -13,3 +13,29 @@ pub fn main() {
             + WINDOW
     );
 }
+
+// pub fn main() {
+//     let b = include_bytes!("../input.txt").map(|c| c - b'a');
+//     let mut counts: [u8; 26] = [0; 26];
+//     let mut b_iter = b.windows(WINDOW).enumerate();
+//     let mut has_dup = false;
+//     for c in b_iter.next().unwrap().1.iter().skip(1) {
+//         counts[*c as usize] += 1;
+//         has_dup |= counts[*c as usize] > 1;
+//     }
+//     for (i, w) in b_iter {
+//         counts[w[WINDOW - 1] as usize] += 1;
+//         has_dup |= counts[w[WINDOW - 1] as usize] > 1;
+
+//         // println!("{} {} {} {:?}", i+1, has_dup, std::str::from_utf8(&w.iter().map(|c| c + b'a').collect::<Vec<u8>>()).unwrap(), counts);
+//         if !has_dup {
+//             println!("{}", i + WINDOW);
+//             break;
+//         }
+
+//         counts[w[0] as usize] -= 1;
+//         if counts[w[0] as usize] == 1 {
+//             has_dup = counts.iter().any(|&c| c > 1)
+//         }
+//     }
+// }
