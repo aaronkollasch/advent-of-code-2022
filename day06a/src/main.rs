6176b1d606aa79426e3e86e8e0724e64dc2b4aff
Aaron Kollasch <aaron@kollasch.dev>
2022-12-06 01:20:50 -0500

    Speedup day 6, 42 -> 19 µs total

--- day06a/src/main.rs
+++ day06a/src/main.rs
@@ -1,43 +1,15 @@
-use std::str;
-use std::cmp;
-
-const WINDOW: usize = 3;
+const WINDOW: usize = 4;
 
 pub fn main() {
-    let b = include_bytes!("../input.txt");
-    let mut buf: [u8; WINDOW] = [0; WINDOW];
-    buf.clone_from_slice(&b[0..WINDOW]);
-    let mut idx: usize = 0;
-    let mut skip_count = WINDOW;
-    let mut match_idx = 0;
-    for (i, c) in b.iter().enumerate() {
-        // println!("{} {} {} {}", i, c, skip_count, str::from_utf8(&buf).unwrap());
-        for j in 0..WINDOW {
-            if *c == buf[(idx+j) % WINDOW] {
-                skip_count = cmp::max(skip_count, j+1)
-            }
-        }
-
-        // if *c == buf[(idx + 2) % 3] {
-        //     skip_count = 3;
-        // } else if *c == buf[(idx + 1) % 3] {
-        //     skip_count = cmp::max(skip_count, 2);
-        // } else if *c == buf[idx] {
-        //     skip_count = cmp::max(skip_count, 1);
-        // } else if skip_count > 0 {
-        //     skip_count -= 1;
-        // }
-        // println!("{} {} {} {}", i, c, skip_count, str::from_utf8(&buf).unwrap());
-        if skip_count == 0 {
-            match_idx = i;
-            break;
-        }
-        if skip_count > 0 {
-            skip_count -= 1;
-        }
-        buf[idx] = *c;
-        idx = (idx + 1) % WINDOW;
-    }
-    println!("{} {}", match_idx+1, str::from_utf8(&b[match_idx-WINDOW..match_idx+1]).unwrap());
-    // println!("{}", match_idx+1);
+    println!(
+        "{}",
+        include_bytes!("../input.txt")
+            .windows(WINDOW)
+            .enumerate()
+            // https://stackoverflow.com/a/46766782/653173
+            .find(|(_, s)| !(1..s.len()).any(|i| s[i..].contains(&s[i - 1])))
+            .unwrap()
+            .0
+            + WINDOW
+    );
 }
--- day06b/src/main.rs
+++ day06b/src/main.rs
@@ -1,32 +1,15 @@
-use std::cmp;
-
-const WINDOW: usize = 14-1;
+const WINDOW: usize = 14;
 
 pub fn main() {
-    let b = include_bytes!("../input.txt");
-    let mut buf: [u8; WINDOW] = [0; WINDOW];
-    buf.clone_from_slice(&b[0..WINDOW]);
-    let mut idx: usize = 0;
-    let mut skip_count = WINDOW;
-    let mut match_idx = 0;
-    for (i, c) in b.iter().enumerate() {
-        // println!("{} {} {} {}", i, c, skip_count, std::str::from_utf8(&buf).unwrap());
-        for j in 0..WINDOW {
-            if *c == buf[(idx+j) % WINDOW] {
-                skip_count = cmp::max(skip_count, j+1)
-            }
-        }
-        // println!("{} {} {} {}", i, c, skip_count, std::str::from_utf8(&buf).unwrap());
-        if skip_count == 0 {
-            match_idx = i;
-            break;
-        }
-        if skip_count > 0 {
-            skip_count -= 1;
-        }
-        buf[idx] = *c;
-        idx = (idx + 1) % WINDOW;
-    }
-    println!("{} {}", match_idx+1, std::str::from_utf8(&b[match_idx-WINDOW..match_idx+1]).unwrap());
-    // println!("{}", match_idx+1);
+    println!(
+        "{}",
+        include_bytes!("../input.txt")
+            .windows(WINDOW)
+            .enumerate()
+            // https://stackoverflow.com/a/46766782/653173
+            .find(|(_, s)| !(1..s.len()).any(|i| s[i..].contains(&s[i - 1])))
+            .unwrap()
+            .0
+            + WINDOW
+    );
 }
