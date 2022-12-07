5ed30a960783cb768f233f79717c157a77646e24
Aaron Kollasch <aaron@kollasch.dev>
2022-12-07 14:23:13 -0500

    Convert day 7 to bytes, 49 µs -> 24 µs total

--- day07a/src/main.rs
+++ day07a/src/main.rs
@@ -1,41 +1,39 @@
-use std::collections::HashMap;
-
 pub fn main() {
-    let s = "\n".to_owned() + include_str!("../input.txt");
+    let b = include_bytes!("../input.txt");
     let mut wd = Vec::<u32>::with_capacity(32);
     wd.push(0);
-    // let mut sizes = Vec::<u32>::with_capacity(256);
     let mut total_size = 0;
 
-    s.split("\n$ ")
-        .filter(|g| g.len() > 2)
-        // .take(10)
-        .for_each(|g| match &g[0..1] {
-            "c" => match &g[3..4] {
-                "." => {
-                    // sizes.push(wd.pop().unwrap());
-                    let s = wd.pop().unwrap();
-                    if s <= 100_000 { total_size += s; }
+    b.split(|x| x == &b'\n')
+        .filter(|l| l.len() > 2)
+        .for_each(|l| match &l[0..3] {
+            b"$ c" => {
+                match l[5] {
+                    b'.' => {
+                        let s = wd.pop().unwrap();
+                        if s <= 100_000 {
+                            total_size += s;
+                        }
+                        *wd.last_mut().unwrap() += s;
+                    }
+                    _ => {
+                        wd.push(0);
+                    }
                 }
-                _ => {
-                    wd.push(0);
-                }
-            },
-            "l" => {
-                let s = g
-                    .lines()
-                    .skip(1)
-                    .filter(|l| l.len() > 0 && &l[0..1] != "d")
-                    .filter_map(|l| l.split_once(" ").unwrap().0.parse::<u32>().ok())
-                    .sum::<u32>();
-                // println!("{}\n{} {}", g, s, wd.join("/"));
-                wd.iter_mut().for_each(|x| *x += s);
+            }
+            _ if l[0] >= b'0' && l[0] <= b'9' => {
+                *wd.last_mut().unwrap() += atoi::atoi::<u32>(l.split(|b| b == &b' ').next().unwrap()).unwrap()
             }
             _ => assert!(true),
         });
 
-    println!(
-        "{}",
-        total_size
-    );
+    let mut size = 0;
+    wd.into_iter().rev().for_each(|x| {
+        let s = x + size;
+        if s <= 100_000 {
+            total_size += s;
+        }
+        size = s;
+    });
+    println!("{}", total_size);
 }
--- day07b/src/main.rs
+++ day07b/src/main.rs
@@ -1,50 +1,43 @@
-use std::collections::HashMap;
-
 pub fn main() {
-    let s = "\n".to_owned() + include_str!("../input.txt");
-    let mut wd = Vec::<u32>::new();
+    let b = include_bytes!("../input.txt");
+    let mut wd = Vec::<u32>::with_capacity(32);
+    wd.push(0);
     let mut sizes = Vec::<u32>::with_capacity(256);
 
-    s.split("\n$ ")
-        .filter(|g| g.len() > 2)
-        .for_each(|g| match &g[0..1] {
-            "c" => match &g[3..4] {
-                "." => {
-                    sizes.push(wd.pop().unwrap());
+    b.split(|x| x == &b'\n')
+        .filter(|l| l.len() > 2)
+        .for_each(|l| match &l[0..3] {
+            b"$ c" => {
+                match l[5] {
+                    b'.' => {
+                        let s = wd.pop().unwrap();
+                        sizes.push(s);
+                        *wd.last_mut().unwrap() += s;
+                    }
+                    _ => {
+                        wd.push(0);
+                    }
                 }
-                _ => {
-                    wd.push(0);
-                }
-            },
-            "l" => {
-                let s = g
-                    .lines()
-                    .skip(1)
-                    .filter(|l| l.len() > 0 && &l[0..1] != "d")
-                    .filter_map(|l| l.split_once(" ").unwrap().0.parse::<u32>().ok())
-                    .sum::<u32>();
-                // println!("{}\n{} {}", g, s, wd.join("/"));
-                wd.iter_mut().for_each(|x| *x += s);
+            }
+            _ if l[0] >= b'0' && l[0] <= b'9' => {
+                *wd.last_mut().unwrap() += atoi::atoi::<u32>(l.split(|b| b == &b' ').next().unwrap()).unwrap()
             }
             _ => assert!(true),
         });
 
-    wd.into_iter().rev().for_each(|x| sizes.push(x));
+    let mut size = 0;
+    wd.into_iter().rev().for_each(|x| {
+        let s = x + size;
+        sizes.push(s);
+        size = s;
+    });
+
     let free_space = 70000000 - sizes.last().unwrap();
     let to_delete = 30000000 - free_space;
     // println!("{} {} {}", total_size, free_space, to_delete);
-    // for (key, val) in sizes.iter() {
-    //     if key.chars().filter(|c| *c == '/').count() <= 1 {
-    //         println!("key: {key} val: {val}");
-    //     }
-    // }
 
     println!(
         "{}",
-        sizes
-            .into_iter()
-            .filter(|s| *s >= to_delete)
-            .min()
-            .unwrap()
+        sizes.into_iter().filter(|s| *s >= to_delete).min().unwrap()
     );
 }
