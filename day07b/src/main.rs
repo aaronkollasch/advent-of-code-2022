e5a03c4c41871c04432bf1935875f9cfd22b6811
Aaron Kollasch <aaron@kollasch.dev>
2022-12-07 15:10:03 -0500

    Simplify day 7

--- day07b/src/main.rs
+++ day07b/src/main.rs
@@ -6,18 +6,14 @@ pub fn main() {
 
     b.split(|x| x == &b'\n')
         .filter(|l| l.len() > 2)
-        .for_each(|l| match &l[0..3] {
-            b"$ c" => {
-                match l[5] {
-                    b'.' => {
-                        let s = wd.pop().unwrap();
-                        sizes.push(s);
-                        *wd.last_mut().unwrap() += s;
-                    }
-                    _ => {
-                        wd.push(0);
-                    }
-                }
+        .for_each(|l| match l {
+            b"$ cd .." => {
+                let s = wd.pop().unwrap();
+                sizes.push(s);
+                *wd.last_mut().unwrap() += s;
+            }
+            _ if &l[0..3] == b"$ c" => {
+                wd.push(0);
             }
             _ if l[0] >= b'0' && l[0] <= b'9' => {
                 *wd.last_mut().unwrap() += atoi::atoi::<u32>(l.split(|b| b == &b' ').next().unwrap()).unwrap()
