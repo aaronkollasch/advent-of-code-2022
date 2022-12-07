189e1ffaeec40d899e3934173a3c44202edbeafe
Aaron Kollasch <aaron@kollasch.dev>
2022-12-07 16:44:04 -0500

    Speedup day 7, 22 µs -> 21 µs total

--- day07a/src/main.rs
+++ day07a/src/main.rs
@@ -1,12 +1,17 @@
 pub fn main() {
     let b = include_bytes!("../input.txt");
     let mut wd = Vec::<u32>::with_capacity(16);
-    wd.push(0);
     let mut total_size = 0;
 
     b.split(|x| x == &b'\n')
         .filter(|l| l.len() > 2)
         .for_each(|l| match l {
+            _ if l[0] >= b'0' && l[0] <= b'9' => {
+                *wd.last_mut().unwrap() += l
+                    .iter()
+                    .take_while(|x| **x != b' ')
+                    .fold(0, |acc, x| acc * 10 + (x - b'0') as u32);
+            }
             b"$ cd .." => {
                 let s = wd.pop().unwrap();
                 if s <= 100_000 {
@@ -16,12 +21,6 @@ pub fn main() {
             }
             _ if &l[0..3] == b"$ c" => {
                 wd.push(0);
-            }
-            _ if l[0] >= b'0' && l[0] <= b'9' => {
-                *wd.last_mut().unwrap() += l
-                    .iter()
-                    .take_while(|x| **x != b' ')
-                    .fold(0, |acc, x| acc * 10 + (x - b'0') as u32);
             }
             _ => assert!(true),
         });
