c2134eb4a6ff868b57ec54d4496255ce4690c918
Aaron Kollasch <aaron@kollasch.dev>
2022-12-08 17:01:44 -0500

    Speedup day 8, 148/236 µs -> 51/143 µs

--- day08a/src/main.rs
+++ day08a/src/main.rs
@@ -1,18 +1,18 @@
 use std::collections::HashSet;
 
+const MAX_L: usize = 99;
+
 pub fn main() {
     let b = include_bytes!("../input.txt");
     let l = b.iter().take_while(|x| **x != b'\n').count();
-    let mut visible: HashSet<(usize, usize)> = HashSet::new();
-
+    let mut visible: HashSet<usize> = HashSet::with_capacity(l * l / 2);
     let mut row_max = 0;
-    let mut col_max: Vec<u8> = vec![0; l];
+    let mut col_max: [u8; MAX_L] = [0; MAX_L];
     b.iter()
         .take((l + 1) * (l + 1) - 1)
         .enumerate()
         .for_each(|(i, c)| {
-            let (x, y) = (i % (l + 1), i / (l + 1));
-            // println!("{} {} {}", x, y, c);
+            let x = i % (l + 1);
             match x {
                 0 => row_max = 0,
                 _ if x >= l => return,
@@ -20,23 +20,22 @@ pub fn main() {
             }
             if *c > row_max {
                 row_max = *c;
-                visible.insert((x, y));
+                visible.insert(i);
             }
             if *c > col_max[x] {
                 col_max[x] = *c;
-                visible.insert((x, y));
+                visible.insert(i);
             }
         });
     col_max.iter_mut().for_each(|x| *x = 0);
     b.iter()
         .take((l + 1) * (l + 1) - 1)
-        .rev()
         .enumerate()
+        .rev()
         .for_each(|(i, c)| {
-            let (x, y) = (i % (l + 1), i / (l + 1));
-            // println!("{} {} {}", x, y, c);
+            let x = i % (l + 1);
             match x {
-                0 => row_max = 0,
+                _ if x == l - 1 => row_max = 0,
                 _ if x >= l => return,
                 _ => {}
             }
@@ -45,19 +44,21 @@ pub fn main() {
             }
             if *c > row_max {
                 row_max = *c;
-                visible.insert((l - x - 1, l - y - 1));
+                visible.insert(i);
             }
             if *c > col_max[x] {
                 col_max[x] = *c;
-                visible.insert((l - x - 1, l - y - 1));
+                visible.insert(i);
             }
         });
 
     #[cfg(debug_assertions)]
     {
-        println!("{:?}", visible);
         let mut matrix: Vec<Vec<u8>> = vec![vec![0; l]; l];
-        visible.iter().for_each(|(x, y)| matrix[*y][*x] = 1);
+        visible.iter().for_each(|i| {
+            let (x, y) = (*i % (l + 1), *i / (l + 1));
+            matrix[y][x] = 1;
+        });
         matrix.iter().for_each(|l| println!("{:?}", l));
     };
     println!("{}", visible.len());
--- day08b/src/main.rs
+++ day08b/src/main.rs
@@ -1,17 +1,16 @@
+const MAX_L: usize = 99;
+
 pub fn main() {
     let b = include_bytes!("../input.txt");
     let l = b.iter().take_while(|x| **x != b'\n').count();
     let mut scores: Vec<usize> = vec![1; l * l];
-
-    let mut row_idx: Vec<usize> = vec![0; 10];
-    let mut col_idx: Vec<Vec<usize>> = vec![vec![0; 10]; l];
+    let mut row_idx: [usize; 10] = [0; 10];
+    let mut col_idx: [[usize; 10]; MAX_L] = [[0; 10]; MAX_L];
     b.iter()
         .take((l + 1) * (l + 1) - 1)
         .enumerate()
         .for_each(|(i, c)| {
             let (x, y) = (i % (l + 1), i / (l + 1));
-            let c_i = (c - b'0') as usize;
-            // println!("{} {} {}", x, y, c);
             match x {
                 0 => {
                     row_idx.iter_mut().for_each(|x| *x = 0);
@@ -21,6 +20,7 @@ pub fn main() {
                 }
                 _ => {}
             }
+            let c_i = (c - b'0') as usize;
             let (view_x, view_y) = (x - row_idx[c_i], y - col_idx[x][c_i]);
             scores[y * l + x] *= view_x * view_y;
             row_idx.iter_mut().take(c_i + 1).for_each(|idx| *idx = x);
@@ -35,8 +35,6 @@ pub fn main() {
         .enumerate()
         .for_each(|(i, c)| {
             let (x, y) = (i % (l + 1), i / (l + 1));
-            let c_i = (c - b'0') as usize;
-            // println!("{} {} {}", x, y, c);
             match x {
                 0 => {
                     row_idx.iter_mut().for_each(|x| *x = 0);
@@ -46,13 +44,13 @@ pub fn main() {
                 }
                 _ => {}
             }
+            let c_i = (c - b'0') as usize;
             let (view_x, view_y) = (x - row_idx[c_i], y - col_idx[x][c_i]);
             scores[(l - y - 1) * l + (l - x - 1)] *= view_x * view_y;
             row_idx.iter_mut().take(c_i + 1).for_each(|idx| *idx = x);
             col_idx[x].iter_mut().take(c_i + 1).for_each(|idx| *idx = y);
         });
 
-    // scores.iter().for_each(|l| println!("{:?}", l));
     #[cfg(debug_assertions)]
     scores.iter().enumerate().for_each(|(i, s)| {
         if i % l == 0 {
