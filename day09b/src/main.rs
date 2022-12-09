b3b5d73f8bf32c5e17611b81749b8c93f02422ef
Aaron Kollasch <aaron@kollasch.dev>
2022-12-09 09:54:10 -0500

    Speedup day 9b, 298 µs -> 132 µs

--- day09b/src/main.rs
+++ day09b/src/main.rs
@@ -1,37 +1,42 @@
-use std::collections::HashSet;
-
 const ROPE_LEN: usize = 10;
 
 pub fn main() {
-    let s = include_bytes!("../input.txt");
-    let mut rope: [(i16, i16); ROPE_LEN] = [(0, 0); ROPE_LEN];
-    let mut visited: HashSet<(i16, i16)> = HashSet::with_capacity(8192);
-
-    s.split(|b| *b == b'\n')
-        .filter(|l| l.len() >= 3)
-        .for_each(|l| {
-            let dir = match l[0] {
-                b'U' => (0, 1),
-                b'D' => (0, -1),
-                b'L' => (-1, 0),
-                b'R' => (1, 0),
-                _ => (0, 0),
-            };
-            let dist = l[2..].iter().fold(0, |acc, x| acc * 10 + (x - b'0') as u8);
-            for _i_step in 0..dist {
-                rope[0].0 += dir.0;
-                rope[0].1 += dir.1;
-                for j in 1..ROPE_LEN {
-                    let head = rope[j - 1];
-                    let tail = rope[j];
-                    if head.0.abs_diff(tail.0).max(head.1.abs_diff(tail.1)) > 1 {
-                        rope[j].0 += (head.0 - tail.0).signum();
-                        rope[j].1 += (head.1 - tail.1).signum();
-                    }
-                }
-                visited.insert(rope[ROPE_LEN - 1]);
+    let cmds = include_bytes!("../input.txt")
+        .split(|b| b == &b'\n')
+        .map(|l| {
+            match (
+                l[0],
+                l[2..].iter().fold(0, |acc, x| acc * 10 + (x - b'0') as u8),
+            ) {
+                (b'U', l) => ((0, -1), l),
+                (b'D', l) => ((0, 1), l),
+                (b'L', l) => ((-1, 0), l),
+                (_, l) => ((1, 0), l),
             }
         });
+    let mut rope: [(i32, i32); ROPE_LEN] = [(0, 0); ROPE_LEN];
+    let mut visited: rustc_hash::FxHashSet<_> = Default::default();
+    visited.reserve(8192);
+    visited.insert((0, 0));
+
+    for (dir, dist) in cmds {
+        for _i_step in 0..dist {
+            rope[0].0 += dir.0;
+            rope[0].1 += dir.1;
+            for j in 1..ROPE_LEN {
+                let head = rope[j - 1];
+                let tail = rope[j];
+                if head.0.abs_diff(tail.0).max(head.1.abs_diff(tail.1)) > 1 {
+                    rope[j].0 += (head.0 - tail.0).signum();
+                    rope[j].1 += (head.1 - tail.1).signum();
+                    (j == ROPE_LEN - 1).then(|| visited.insert(rope[ROPE_LEN - 1]));
+                } else {
+                    break;
+                }
+            }
+        }
+    }
+
     #[cfg(debug_assertions)]
     println!(
         "{} {}",
