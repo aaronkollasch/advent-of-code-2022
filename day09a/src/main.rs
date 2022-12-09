2ed793fea3f3abeb8dc3c96fee1e57bf0d9b2304
Aaron Kollasch <aaron@kollasch.dev>
2022-12-09 09:43:02 -0500

    Speedup day 9a, 136 µs -> 71 µs

--- day09a/src/main.rs
+++ day09a/src/main.rs
@@ -1,33 +1,32 @@
-use std::collections::HashSet;
-
 pub fn main() {
-    let s = include_bytes!("../input.txt");
-    let (mut hx, mut hy): (i16, i16) = (0, 0);
-    let (mut tx, mut ty): (i16, i16) = (0, 0);
-    let mut visited: HashSet<(i16, i16)> = HashSet::with_capacity(8192);
-    visited.insert((0, 0));
-
-    s.split(|b| *b == b'\n')
-        .filter(|l| l.len() >= 3)
-        .for_each(|l| {
-            let dir = match l[0] {
-                b'U' => (0, 1),
-                b'D' => (0, -1),
-                b'L' => (-1, 0),
-                b'R' => (1, 0),
-                _ => (0, 0),
-            };
-            let dist = l[2..].iter().fold(0, |acc, x| acc * 10 + (x - b'0') as u8);
-            for _i_step in 0..dist {
-                hx += dir.0;
-                hy += dir.1;
-                if hx.abs_diff(tx).max(hy.abs_diff(ty)) > 1 {
-                    tx += (hx - tx).signum();
-                    ty += (hy - ty).signum();
-                    visited.insert((tx, ty));
-                }
+    let cmds = include_bytes!("../input.txt")
+        .split(|b| b == &b'\n')
+        .map(|l| {
+            match (
+                l[0],
+                l[2..].iter().fold(0, |acc, x| acc * 10 + (x - b'0') as u8),
+            ) {
+                (b'U', l) => ((0, -1), l),
+                (b'D', l) => ((0, 1), l),
+                (b'L', l) => ((-1, 0), l),
+                (_, l) => ((1, 0), l),
             }
         });
+    let (mut h, mut t, mut visited): ((i32, i32), (_, _), rustc_hash::FxHashSet<_>) =
+        Default::default();
+    visited.reserve(8192);
+    visited.insert((0, 0));
+
+    for (dir, dist) in cmds {
+        for _i_step in 0..dist {
+            h = (h.0 + dir.0, h.1 + dir.1);
+            if h.0.abs_diff(t.0) > 1 || h.1.abs_diff(t.1) > 1 {
+                t = (h.0 - dir.0, h.1 - dir.1);
+                visited.insert(t);
+            }
+        }
+    }
+
     #[cfg(debug_assertions)]
     println!(
         "{} {}",
