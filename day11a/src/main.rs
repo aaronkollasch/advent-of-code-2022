c0c19f2942e178b44821c22f8f6b94ce43148354
Aaron Kollasch <aaron@kollasch.dev>
2022-12-11 05:10:32 -0500

    Switch day 11a parsing to bytes, 6 µs -> 5 µs

--- day11a/src/main.rs
+++ day11a/src/main.rs
@@ -8,59 +8,78 @@ struct Monkey {
 }
 
 pub fn main() {
-    let s = include_str!("../input.txt");
+    let s = include_bytes!("../input.txt");
     let mut monkeys: Vec<Monkey> = Vec::<Monkey>::new();
 
     // parse input
-    s.split("\n\n").enumerate().for_each(|(i_monkey, m)| {
-        let mut monkey: Monkey = Monkey {
-            items: Vec::with_capacity(32),
-            test: 0,
-            operation: (0, 0),
-            target1: 0,
-            target2: 0,
-            inspect_count: 0,
-        };
-        for (i, l) in m.lines().enumerate() {
-            match i {
-                1 => {
-                    monkey.items.extend(
-                        l.split_once(": ")
-                            .unwrap()
-                            .1
-                            .split(", ")
-                            .map(|w| w.parse::<u64>().expect(w)),
+    for (i_line, l) in s.split(|b| *b == b'\n').filter(|l| l.len() > 0).enumerate() {
+        let (i_monkey, i_line) = (i_line / 6, i_line % 6);
+        if i_line == 0 {
+            monkeys.push(Monkey {
+                items: Vec::with_capacity(32),
+                test: 0,
+                operation: (0, 0),
+                target1: 0,
+                target2: 0,
+                inspect_count: 0,
+            });
+        }
+        let mut monkey = monkeys
+            .get_mut(i_monkey)
+            .unwrap_or_else(|| panic!("{}", i_monkey));
+        match i_line {
+            1 => {
+                monkey.items.extend(
+                    l[17..]
+                        .split(|b| *b == b',')
+                        .map(|w| w[1..].iter().fold(0, |acc, x| acc * 10 + (x - b'0') as u64)),
+                );
+            }
+            2 => {
+                if l[23] == b'+' {
+                    monkey.operation = (
+                        0,
+                        l[25..]
+                            .iter()
+                            .fold(0, |acc, x| acc * 10 + (x - b'0') as u64),
+                    );
+                } else if l[25] == b'o' {
+                    monkey.operation = (2, 0);
+                } else {
+                    monkey.operation = (
+                        1,
+                        l[25..]
+                            .iter()
+                            .fold(0, |acc, x| acc * 10 + (x - b'0') as u64),
                     );
                 }
-                2 => {
-                    if l.chars().nth(23) == Some('+') {
-                        monkey.operation = (0, l[25..].parse::<u64>().unwrap());
-                    } else if &l[25..] == "old" {
-                        monkey.operation = (2, 0);
-                    } else {
-                        monkey.operation = (1, l[25..].parse::<u64>().unwrap());
-                    }
-                }
-                3 => {
-                    monkey.test = l[21..].parse::<u64>().unwrap();
-                }
-                4 => {
-                    monkey.target1 = l[29..].parse::<usize>().unwrap();
-                    if i_monkey == monkey.target1 {
-                        panic!("monkey {} targets itself!", i_monkey);
-                    }
-                }
-                5 => {
-                    monkey.target2 = l[30..].parse::<usize>().unwrap();
-                    if i_monkey == monkey.target2 {
-                        panic!("monkey {} targets itself!", i_monkey);
-                    }
-                }
-                _ => {}
             }
+            3 => {
+                monkey.test = l[21..]
+                    .iter()
+                    .fold(0, |acc, x| acc * 10 + (x - b'0') as u64);
+            }
+            4 => {
+                monkey.target1 = l[29..]
+                    .iter()
+                    .fold(0, |acc, x| acc * 10 + (x - b'0') as usize);
+                if i_monkey == monkey.target1 {
+                    panic!("monkey {} targets itself!", i_monkey);
+                }
+            }
+            5 => {
+                monkey.target2 = l[30..]
+                    .iter()
+                    .fold(0, |acc, x| acc * 10 + (x - b'0') as usize);
+                if i_monkey == monkey.target2 {
+                    panic!("monkey {} targets itself!", i_monkey);
+                }
+            }
+            _ => {}
         }
-        monkeys.push(monkey);
-    });
+    }
+    #[cfg(debug_assertions)]
+    eprintln!("monkeys: {}", monkeys.len());
 
     // simulate rounds
     for _round in 1..21 {
