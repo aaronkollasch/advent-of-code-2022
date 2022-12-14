be7c5b0a179b5112b3bff0fa0e3b59360a2c51bf
Aaron Kollasch <aaron@kollasch.dev>
2022-12-13 23:56:41 -0500

    Remove collect from day 3b, saves ~1 µs

--- day03b/src/main.rs
+++ day03b/src/main.rs
@@ -1,15 +1,23 @@
+use itertools::Itertools;
+
 pub fn main() {
     print!(
         "{} ",
         include_bytes!("../input.txt")
             .split(|b| *b == b'\n')
             .filter(|l| l.len() > 0)
-            .collect::<Vec<_>>()
             .chunks(3)
-            .map(|group| group[0]
-                .iter()
-                .find(|item| group[1].contains(item) && group[2].contains(item))
-                .unwrap())
+            .into_iter()
+            .map(|mut group| {
+                let (a, b, c) = (
+                    group.next().unwrap(),
+                    group.next().unwrap(),
+                    group.next().unwrap(),
+                );
+                a.iter()
+                    .find(|item| b.contains(*item) && c.contains(*item))
+                    .unwrap()
+            })
             .map(|b| {
                 if *b >= b'a' {
                     (b - b'a') as u16 + 1
