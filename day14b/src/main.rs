231dcd27dd06cb9f5dc17cb997c34fb640590e70
Aaron Kollasch <aaron@kollasch.dev>
2022-12-14 03:03:14 -0500

    Make day 14b map array one-dimensional
    Speedup 161 µs -> 153 µs

--- day14b/src/main.rs
+++ day14b/src/main.rs
@@ -1,16 +1,26 @@
 use itertools::Itertools;
 use std::cmp::{min, max};
 
-#[derive(Debug, Clone, Copy, Hash, PartialEq, Eq)]
-struct Pos {
-    pub x: usize,
-    pub y: usize,
+type Pos = usize;
+
+#[inline]
+fn pos(x: usize, y: usize) -> Pos {
+    y * MAP_WIDTH + x
+}
+
+#[inline]
+fn x(p: Pos) -> usize {
+    p % MAP_WIDTH
+}
+
+#[inline]
+fn y(p: Pos) -> usize {
+    p / MAP_WIDTH
 }
 
 const MAP_AIR: u8 = b' ';
 const MAP_ROCK: u8 = b'#';
 const MAP_SAND: u8 = b'o';
-const DROP_POS: Pos = Pos { x: 500, y: 0 };
 const Y_MAX: usize = 157 + 2;
 #[cfg(debug_assertions)]
 const X_MIN_TRUE: usize = 491 - Y_MAX - 1;
@@ -19,6 +29,7 @@ const X_MAX: usize = 561 + Y_MAX + 1;
 const MAP_WIDTH: usize = X_MAX - X_MIN + 1;
 const MAP_HEIGHT: usize = Y_MAX + 1;
 const MAP_SIZE: usize = MAP_WIDTH * MAP_HEIGHT;
+const DROP_POS: Pos = 500 - X_MIN;
 
 #[derive(Debug, Clone)]
 struct Map {
@@ -32,23 +43,25 @@ impl Map {
 
     #[inline]
     pub fn get_val(&self, pos: Pos) -> u8 {
-        self.contents[pos.y * MAP_WIDTH + pos.x]
+        self.contents[pos]
     }
 
     #[inline]
     pub fn set_val(&mut self, pos: Pos, val: u8) {
-        self.contents[pos.y * MAP_WIDTH + pos.x] = val;
+        self.contents[pos] = val;
     }
 
     #[inline]
     pub fn fill_line(&mut self, pos1: Pos, pos2: Pos, val: u8) {
-        if pos1.x == pos2.x {
-            for y in min(pos1.y, pos2.y)..=max(pos1.y, pos2.y) {
-                self.set_val(Pos { x: pos1.x, y }, val);
+        if x(pos1) == x(pos2) {
+            let pos = min(pos1, pos2);
+            for y in 0..=y(pos1.abs_diff(pos2)) {
+                self.set_val(pos + y * MAP_WIDTH, val);
             }
-        } else if pos1.y == pos2.y {
-            for x in min(pos1.x, pos2.x)..=max(pos1.x, pos2.x) {
-                self.set_val(Pos { x, y: pos1.y }, val);
+        } else if y(pos1) == y(pos2) {
+            let pos = min(pos1, pos2);
+            for x in 0..=pos1.abs_diff(pos2) {
+                self.set_val(pos + x, val);
             }
         }
     }
@@ -58,20 +71,20 @@ impl Map {
         if drop_path.len() == 0 { return false; }
         loop {
             let pos = *drop_path.last().unwrap();
-            if pos.y >= MAP_HEIGHT - 1 {
+            if y(pos) >= MAP_HEIGHT - 1 {
                 return false;
             }
-            let new_pos = Pos { x: pos.x, y: pos.y + 1};
+            let mut new_pos = pos + MAP_WIDTH;
             if self.get_val(new_pos) == MAP_AIR {
                 drop_path.push(new_pos);
                 continue;
             }
-            let new_pos = Pos { x: pos.x - 1, y: pos.y + 1};
+            new_pos -= 1;
             if self.get_val(new_pos) == MAP_AIR {
                 drop_path.push(new_pos);
                 continue;
             }
-            let new_pos = Pos { x: pos.x + 1, y: pos.y + 1};
+            new_pos += 2;
             if self.get_val(new_pos) == MAP_AIR {
                 drop_path.push(new_pos);
                 continue;
@@ -87,10 +100,9 @@ impl Map {
 #[inline]
 fn parse_point(p: &str) -> Pos {
     let (x, y) = p.split_once(',').unwrap();
-    Pos {
-        x: x.parse().unwrap_or_else(|_| panic!("parse failed x: {}", x)),
-        y: y.parse().unwrap_or_else(|_| panic!("parse failed y: {}", y)),
-    }
+    let x = x.parse::<usize>().unwrap_or_else(|_| panic!("parse failed x: {}", x));
+    let y = y.parse::<usize>().unwrap_or_else(|_| panic!("parse failed y: {}", y));
+    y * MAP_WIDTH + x
 }
 
 pub fn main() {
@@ -102,17 +114,17 @@ pub fn main() {
     s.lines().for_each(|l| {
         for (prev, next) in l.split(" -> ").map(parse_point).tuple_windows() {
             map.fill_line(prev, next, MAP_ROCK);
-            y_max = max(y_max, max(prev.y, next.y));
+            y_max = max(y_max, max(y(prev), y(next)));
             #[cfg(debug_assertions)]
             {
-                x_min = min(x_min, min(prev.x, next.x));
-                x_max = max(x_max, max(prev.x, next.x));
+                x_min = min(x_min, min(x(prev), x(next)));
+                x_max = max(x_max, max(x(prev), x(next)));
             }
         }
     });
     #[cfg(debug_assertions)]
     println!("y_max: {}, x_min: {}, x_max: {}", y_max, x_min, x_max);
-    map.fill_line(Pos { x: X_MIN, y: y_max + 2 }, Pos { x: X_MAX, y: y_max + 2 }, MAP_ROCK);
+    map.fill_line(pos(X_MIN, y_max + 2), pos(X_MAX, y_max + 2), MAP_ROCK);
     let mut i = 0;
     let mut drop_path: Vec<Pos> = Vec::with_capacity(MAP_HEIGHT);
     drop_path.push(DROP_POS);
@@ -120,7 +132,7 @@ pub fn main() {
     #[cfg(debug_assertions)]
     for y in 0..Y_MAX {
         for x in X_MIN_TRUE..X_MAX {
-            print!("{}", char::from_u32(map.get_val(Pos { x, y: y }) as u32).unwrap());
+            print!("{}", char::from_u32(map.get_val(pos(x, y)) as u32).unwrap());
         }
         println!();
     }
