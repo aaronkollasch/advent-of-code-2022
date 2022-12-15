63acdd5fe22544efa861a6f07b6d0f0003aa7d5d
Aaron Kollasch <aaron@kollasch.dev>
2022-12-15 03:29:36 -0500

    Make day 15b parallel, 50 ms -> 19 ms

--- day15b/src/main.rs
+++ day15b/src/main.rs
@@ -1,9 +1,48 @@
 use itertools::Itertools;
 use std::cmp::{max, min};
+use std::thread;
+
+const NUM_ZONES: usize = 32;
+const I_MAX: isize = 4000000;
+const NUM_THREADS: isize = 16;
+const CHUNK_SIZE: isize = I_MAX / NUM_THREADS;
+
+fn find_indices(zones: [(isize, isize, isize); NUM_ZONES], y_min: isize, y_max: isize) -> (isize, isize) {
+    let mut final_y = isize::MIN;
+    let mut final_x = isize::MIN;
+    let mut y = y_min;
+    'outer: while y <= y_max {
+        let mut last_x = 0;
+        let mut min_overlap = isize::MAX;
+        for (start, end) in zones
+            .iter()
+            .filter_map(|(s_x, s_y, beacon_dist)| {
+                let range_width = beacon_dist - s_y.abs_diff(y) as isize;
+                if range_width < 0 {
+                    None
+                } else {
+                    Some((s_x - range_width, s_x + range_width))
+                }
+            })
+            .sorted_unstable()
+        {
+            if start <= last_x + 1 {
+                min_overlap = min(min_overlap, last_x - start + 1);
+                last_x = min(max(last_x, end), I_MAX);
+            } else {
+                final_y = y;
+                final_x = start - 1;
+                break 'outer;
+            }
+        }
+        y += (min_overlap + 1) / 2;
+    }
+    (final_x, final_y)
+}
 
 pub fn main() {
     let s = include_bytes!("../input.txt");
-    let zones: [_; 32] = s
+    let zones: [_; NUM_ZONES] = s
         .split(|b| *b == b'\n')
         .filter(|l| l.len() > 0)
         .map(|l| {
@@ -42,33 +81,19 @@ pub fn main() {
         .unwrap();
     let mut final_y = 0;
     let mut final_x = 0;
-    const I_MAX: isize = 4000000;
-    let mut y = 0;
-    'outer: while y <= I_MAX {
-        let mut last_x = 0;
-        let mut min_overlap = isize::MAX;
-        for (start, end) in zones
-            .iter()
-            .filter_map(|(s_x, s_y, beacon_dist)| {
-                let range_width = beacon_dist - s_y.abs_diff(y) as isize;
-                if range_width < 0 {
-                    None
-                } else {
-                    Some((s_x - range_width, s_x + range_width))
-                }
-            })
-            .sorted_unstable()
-        {
-            if start <= last_x + 1 {
-                min_overlap = min(min_overlap, last_x - start + 1);
-                last_x = min(max(last_x, end), I_MAX);
-            } else {
-                final_y = y;
-                final_x = start - 1;
-                break 'outer;
-            }
-        }
-        y += (min_overlap + 1) / 2;
-    }
+    let handles: Vec<_> = (0..NUM_THREADS).map(|i_thread| {
+        thread::spawn(move || {
+            let y_min = i_thread * CHUNK_SIZE;
+            let y_max = (i_thread + 1) * CHUNK_SIZE;
+            find_indices(zones, y_min, y_max)
+        })
+    }).collect::<Vec<_>>();
+    handles.into_iter().for_each(|handle| {
+        let (x, y) = handle.join().unwrap();
+        final_x = max(final_x, x);
+        final_y = max(final_y, y);
+    });
+    #[cfg(debug_assertions)]
+    println!("{} {}", final_x, final_y);
     print!("{} ", final_y + 4000000 * final_x + final_y);
 }
