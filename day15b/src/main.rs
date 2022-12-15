f3a18df3109bb188b4d3817a9d6e93e1edf1d408
Aaron Kollasch <aaron@kollasch.dev>
2022-12-15 04:04:44 -0500

    Cleanup day 15b

--- day15b/src/main.rs
+++ day15b/src/main.rs
@@ -7,7 +7,11 @@ const I_MAX: isize = 4000000;
 const NUM_THREADS: isize = 16;
 const CHUNK_SIZE: isize = I_MAX / NUM_THREADS;
 
-fn find_indices(zones: [(isize, isize, isize); NUM_ZONES], y_min: isize, y_max: isize) -> (isize, isize) {
+fn find_indices(
+    zones: [(isize, isize, isize); NUM_ZONES],
+    y_min: isize,
+    y_max: isize,
+) -> (isize, isize) {
     let mut final_y = isize::MIN;
     let mut final_x = isize::MIN;
     let mut y = y_min;
@@ -81,18 +85,21 @@ pub fn main() {
         .unwrap();
     let mut final_y = 0;
     let mut final_x = 0;
-    let handles: Vec<_> = (0..NUM_THREADS).map(|i_thread| {
-        thread::spawn(move || {
-            let y_min = i_thread * CHUNK_SIZE;
-            let y_max = (i_thread + 1) * CHUNK_SIZE;
-            find_indices(zones, y_min, y_max)
+    (0..NUM_THREADS)
+        .map(|i_thread| {
+            thread::spawn(move || {
+                let y_min = i_thread * CHUNK_SIZE;
+                let y_max = (i_thread + 1) * CHUNK_SIZE;
+                find_indices(zones, y_min, y_max)
+            })
         })
-    }).collect::<Vec<_>>();
-    handles.into_iter().for_each(|handle| {
-        let (x, y) = handle.join().unwrap();
-        final_x = max(final_x, x);
-        final_y = max(final_y, y);
-    });
+        .collect::<Vec<_>>()
+        .into_iter()
+        .for_each(|handle| {
+            let (x, y) = handle.join().unwrap();
+            final_x = max(final_x, x);
+            final_y = max(final_y, y);
+        });
     #[cfg(debug_assertions)]
     println!("{} {}", final_x, final_y);
     print!("{} ", 4000000 * final_x + final_y);
