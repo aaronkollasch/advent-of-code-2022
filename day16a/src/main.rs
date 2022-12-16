ec681e21272f37ce2a8aed4f348b5560d5c027ed
Aaron Kollasch <aaron@kollasch.dev>
2022-12-16 12:16:17 -0500

    Move day 16b solution to day 16a
    Speedup 141 ms -> 103 ms

--- day16a/src/main.rs
+++ day16a/src/main.rs
@@ -1,12 +1,14 @@
 use itertools::Itertools;
 use std::collections::HashMap;
 
+const NUM_LINES: usize = 60;
+
 fn get_distance<'a>(
     depth: usize,
-    valve1: &'a str,
-    valve2: &'a str,
-    valve_map: &HashMap<&'a str, (isize, Vec<&'a str>)>,
-    mut distance_memo: &mut HashMap<(&'a str, &'a str), isize>,
+    valve1: usize,
+    valve2: usize,
+    valve_map: &HashMap<usize, Vec<usize>>,
+    mut distance_memo: &mut HashMap<(usize, usize), isize>,
 ) -> Option<isize> {
     if valve1 == valve2 {
         return Some(0);
@@ -16,13 +18,12 @@ fn get_distance<'a>(
         return Some(*distance);
     } else if let Some(distance) = distance_memo.get(&(valve2, valve1)) {
         return Some(*distance);
-    } else if valve_map[valve1].1.iter().contains(&valve2) {
+    } else if valve_map[&valve1].iter().contains(&valve2) {
         distance_memo.insert((valve1, valve2), 1);
         return Some(1);
-    } else if let Some(distance) = valve_map[valve1]
-        .1
+    } else if let Some(distance) = valve_map[&valve1]
         .iter()
-        .filter_map(|valve| get_distance(depth + 1, valve, valve2, &valve_map, &mut distance_memo))
+        .filter_map(|valve| get_distance(depth + 1, *valve, valve2, &valve_map, &mut distance_memo))
         .min()
     {
         let distance = distance.saturating_add(1);
@@ -34,47 +35,64 @@ fn get_distance<'a>(
 }
 
 fn calc_opportunity<'a>(
-    current_valve: &'a str,
-    valve_map: &HashMap<&'a str, (isize, Vec<&'a str>)>,
-    open_valves: &Vec<&'a str>,
+    current_valve: usize,
+    valve_map: &HashMap<usize, Vec<usize>>,
+    flow_rates: [isize; NUM_LINES],
+    valve_values: [isize; NUM_LINES],
     available_time: isize,
-    mut distance_memo: &mut HashMap<(&'a str, &'a str), isize>,
-) -> isize {
-    if open_valves.len() == 0 {
-        return 0;
+    mut distance_memo: &mut HashMap<(usize, usize), isize>,
+) -> (isize, Option<[isize; NUM_LINES]>) {
+    #[cfg(debug_assertions)]
+    if available_time >= 20 {
+        println!("{} {}", current_valve, available_time);
     }
     if available_time < 1 {
-        return 0;
+        return (0, Some(valve_values));
     }
-    return open_valves
+    let result = valve_values
         .iter()
         .enumerate()
-        .filter_map(|(i, target_valve)| {
-            let distance = get_distance(0, current_valve, target_valve, &valve_map, &mut distance_memo);
+        .filter(|(target_valve, _is_open)| flow_rates[*target_valve] > 0)
+        .filter_map(|(target_valve, target_cur_value)| {
+            let distance = get_distance(
+                0,
+                current_valve,
+                target_valve,
+                &valve_map,
+                &mut distance_memo,
+            );
             if distance.is_none() {
                 return None;
             }
-            let flow_rate = valve_map[target_valve].0;
             let distance = distance.unwrap();
-            let mut open_valves = open_valves.clone();
-            open_valves.remove(i);
             let time_after_open = available_time - distance - 1;
             if time_after_open < 0 {
                 return None;
             }
+            let flow_rate = flow_rates[target_valve];
+            let valve_value = flow_rate * time_after_open;
+            if valve_value <= *target_cur_value {
+                return None;
+            }
+            let mut valve_values = valve_values.to_owned();
+            valve_values[target_valve] = valve_value;
+            // #[cfg(debug_assertions)]
             // println!("opening {} at {}, {}", target_valve, 31 - time_after_open, flow_rate * time_after_open);
-            Some(
-                calc_opportunity(
-                    target_valve,
-                    &valve_map,
-                    &open_valves,
-                    time_after_open,
-                    &mut distance_memo,
-                ) + flow_rate * time_after_open,
-            )
+            let mut result = calc_opportunity(
+                target_valve,
+                &valve_map,
+                flow_rates,
+                valve_values,
+                time_after_open,
+                &mut distance_memo,
+            );
+            result.0 += valve_value - *target_cur_value;
+            result.1 = result.1.to_owned();
+            Some(result)
         })
-        .max()
-        .unwrap_or(0);
+        .max_by(|a, b| a.0.cmp(&b.0))
+        .unwrap_or((0, Some(valve_values)));
+    result
 }
 
 pub fn main() {
@@ -95,25 +113,52 @@ pub fn main() {
                     .collect::<Vec<_>>(),
                 _ => unreachable!(),
             };
-            (valve, (flow_rate, valves))
+            (valve, flow_rate, valves)
         })
+        .collect::<Vec<_>>();
+    let valve_indices = valve_map
+        .to_owned()
+        .into_iter()
+        .sorted_unstable_by(|a, b| b.1.cmp(&a.1))
+        .enumerate()
+        .map(|(i, (valve, _, _))| (valve, i))
         .collect::<HashMap<_, _>>();
+    #[cfg(debug_assertions)]
+    println!("{:?}", valve_indices);
+
+    let flow_rates = valve_map
+        .iter()
+        .sorted_unstable_by(|a, b| valve_indices[a.0].cmp(&valve_indices[b.0]))
+        .map(|(_valve, flow_rate, _valves)| *flow_rate)
+        .collect::<Vec<_>>()
+        .as_slice()
+        .try_into()
+        .unwrap();
+    #[cfg(debug_assertions)]
+    println!("{:?}", flow_rates);
+
+    let valve_map = valve_map
+        .into_iter()
+        .map(|(valve, _flow_rate, valves)| {
+            (
+                *valve_indices.get(&valve).unwrap(),
+                valves
+                    .into_iter()
+                    .map(|v| *valve_indices.get(&v).unwrap())
+                    .collect::<Vec<usize>>(),
+            )
+        })
+        .collect::<HashMap<usize, Vec<usize>>>();
     #[cfg(debug_assertions)]
     println!("{:?}", valve_map);
 
     let mut distance_memo = &mut HashMap::with_capacity(256);
+    let valve_values: [isize; NUM_LINES] = [0; NUM_LINES];
 
-    let open_valves = valve_map
-        .iter()
-        .filter(|(valve, _state)| valve_map[*valve].0 > 0)
-        .map(|(key, _value)| *key)
-        .sorted_unstable_by(|a, b| valve_map[*a].0.cmp(&valve_map[*b].0))
-        .collect::<Vec<_>>();
+    let result1 = calc_opportunity(valve_indices["AA"], &valve_map, flow_rates, valve_values, 30, &mut distance_memo);
     #[cfg(debug_assertions)]
-    println!("{:?}", open_valves);
-    // for target_valve in open_valves.iter() {
-    //     println!("AA - {}: {}", target_valve, get_distance(0, "AA", target_valve, &valve_map, &mut distance_memo).unwrap());
-    // }
-
-    print!("{} ", calc_opportunity("AA", &valve_map, &open_valves, 30, &mut distance_memo));
+    println!("{}\t{:?}", result1.0, result1.1);
+    print!(
+        "{} ", result1.0
+    );
 }
