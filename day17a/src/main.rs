50ef6e31a63b42887636140fb6d0ad7a22402968
Aaron Kollasch <aaron@kollasch.dev>
2022-12-17 14:46:47 -0500

    Switch day 17 maps to bitvec
    Speedup 220 / 773 µs -> 102 / 372 µs
    https://www.reddit.com/r/adventofcode/comments/znykq2/comment/j0k7szh

--- day17a/src/main.rs
+++ day17a/src/main.rs
@@ -1,65 +1,55 @@
-// chars:
-const AIR: u8 = b' ';
-const ROCK: u8 = b'#';
+type Row = u8;
+type Rock = ([Row; 4], usize);
 
 // rocks:
 //
 // ####
-const ROCK_1: [[u8; 4]; 4] = [
-    [ROCK, ROCK, ROCK, ROCK],
-    [AIR, AIR, AIR, AIR],
-    [AIR, AIR, AIR, AIR],
-    [AIR, AIR, AIR, AIR],
-];
+const ROCK_1: Rock = ([0b0011110, 0, 0, 0], 1);
 // .#.
 // ###
 // .#.
-const ROCK_2: [[u8; 4]; 4] = [
-    [AIR, ROCK, AIR, AIR],
-    [ROCK, ROCK, ROCK, AIR],
-    [AIR, ROCK, AIR, AIR],
-    [AIR, AIR, AIR, AIR],
-];
+const ROCK_2: Rock = ([0b0001000, 0b0011100, 0b0001000, 0], 3);
 // ..#
 // ..#
 // ###
-const ROCK_3: [[u8; 4]; 4] = [
-    [ROCK, ROCK, ROCK, AIR],
-    [AIR, AIR, ROCK, AIR],
-    [AIR, AIR, ROCK, AIR],
-    [AIR, AIR, AIR, AIR],
-];
+const ROCK_3: Rock = ([0b0011100, 0b0000100, 0b0000100, 0], 3);
 // #
 // #
 // #
 // #
-const ROCK_4: [[u8; 4]; 4] = [
-    [ROCK, AIR, AIR, AIR],
-    [ROCK, AIR, AIR, AIR],
-    [ROCK, AIR, AIR, AIR],
-    [ROCK, AIR, AIR, AIR],
-];
+const ROCK_4: Rock = ([0b0010000, 0b0010000, 0b0010000, 0b0010000], 4);
 // ##
 // ##
-const ROCK_5: [[u8; 4]; 4] = [
-    [ROCK, ROCK, AIR, AIR],
-    [ROCK, ROCK, AIR, AIR],
-    [AIR, AIR, AIR, AIR],
-    [AIR, AIR, AIR, AIR],
-];
-const ROCKS: [[[u8; 4]; 4]; 5] = [ROCK_1, ROCK_2, ROCK_3, ROCK_4, ROCK_5];
+const ROCK_5: Rock = ([0b0011000, 0b0011000, 0, 0], 2);
+
+const ROCKS: [Rock; 5] = [ROCK_1, ROCK_2, ROCK_3, ROCK_4, ROCK_5];
 
 const MAP_HEIGHT: usize = 128;
-const MAP_WIDTH: usize = 7;
 
-#[derive(Debug, Clone, Copy, Hash, PartialEq, Eq)]
-struct Pos {
-    x: usize,
-    y: usize,
+fn shift_rock_left(rock: &mut Rock) {
+    if (0..rock.1).map(|i_rock| {
+        rock.0[i_rock] & 0b1000000
+    }).fold(0, |acc, x| acc | x as Row) > 0 {
+        return
+    }
+    for i_rock in 0..rock.1 {
+        rock.0[i_rock] <<= 1;
+    }
+}
+
+fn shift_rock_right(rock: &mut Rock) {
+    if (0..rock.1).map(|i_rock| {
+        rock.0[i_rock] & 0b0000001
+    }).fold(0, |acc, x| acc | x as Row) > 0 {
+        return
+    }
+    for i_rock in 0..rock.1 {
+        rock.0[i_rock] >>= 1;
+    }
 }
 
 struct Map {
-    contents: [[u8; MAP_WIDTH]; MAP_HEIGHT],
+    contents: [Row; MAP_HEIGHT],
     highest_rock: usize,
     map_height: usize,
 }
@@ -67,80 +57,55 @@ struct Map {
 impl Map {
     pub fn new() -> Self {
         Self {
-            contents: [[AIR; MAP_WIDTH]; MAP_HEIGHT],
+            contents: [0; MAP_HEIGHT],
             highest_rock: 0,
             map_height: 20,
         }
     }
 
     #[inline]
-    pub fn get_contents(&self, pos: Pos) -> u8 {
-        self.contents[pos.y % MAP_HEIGHT][pos.x]
+    pub fn get_row(&self, y: usize) -> Row {
+        self.contents[y % MAP_HEIGHT]
     }
 
     #[inline]
-    pub fn set_contents(&mut self, pos: Pos, val: u8) {
-        self.contents[pos.y % MAP_HEIGHT][pos.x] = val;
+    pub fn get_row_mut(&mut self, y: usize) -> &mut Row {
+        &mut self.contents[y % MAP_HEIGHT]
     }
 
     #[inline]
-    pub fn check_bounds(&self, pos: Pos) -> bool {
-        pos.x < MAP_WIDTH && pos.y < self.map_height
+    pub fn set_row(&mut self, y: usize, val: Row) {
+        self.contents[y % MAP_HEIGHT] = val;
     }
 
-    pub fn collides_with(&self, rock: Rock) -> bool {
-        let pos = rock.pos;
-        let rock = ROCKS[rock.class];
-        (0..4)
-            .map(|x| (0..4).map(move |y| (x, y)))
-            .flatten()
-            .any(|(x, y)| {
-                let map_pos: Pos = Pos {
-                    x: pos.x.wrapping_add(x),
-                    y: pos.y.wrapping_add(y),
-                };
-                if self.check_bounds(map_pos) {
-                    rock[y][x] != AIR && self.get_contents(map_pos) != AIR
-                } else {
-                    rock[y][x] != AIR
-                }
+    #[inline]
+    pub fn collides_with(&self, rock: Rock, rock_y: usize) -> bool {
+        if rock_y.saturating_add(rock.1) >= self.map_height { return true; }
+        (rock_y..rock_y + rock.1)
+            .enumerate()
+            .any(|(i_rock, y)| {
+                self.get_row(y) & rock.0[i_rock] > 0
             })
     }
 
-    pub fn add_rock(&mut self, rock: Rock) {
-        let pos = rock.pos;
-        let rock = ROCKS[rock.class];
-        for x in 0..4 {
-            for y in 0..4 {
-                let map_pos: Pos = Pos {
-                    x: pos.x.wrapping_add(x),
-                    y: pos.y.wrapping_add(y),
-                };
-                if rock[y][x] != AIR {
-                    self.set_contents(map_pos, rock[y][x]);
-                }
-            }
-        }
+    pub fn add_rock(&mut self, rock: Rock, rock_y: usize) {
+        (rock_y..rock_y + rock.1)
+            .enumerate()
+            .for_each(|(i_rock, y)| {
+                let row = self.get_row_mut(y);
+                *row |= rock.0[i_rock];
+            });
         for y in self.highest_rock..self.highest_rock + 4 {
-            if (0..MAP_WIDTH).any(|x| self.get_contents(Pos { x, y }) != AIR) {
+            if self.get_row(y) > 0 {
                 self.highest_rock = y + 1;
             }
         }
         for y in self.map_height..self.highest_rock + 20 {
-            for x in 0..MAP_WIDTH {
-                self.set_contents(Pos { x, y }, AIR);
-            }
+            self.set_row(y, 0);
         }
         self.map_height = self.highest_rock + 20;
     }
 }
-
-#[derive(Debug, Clone, Copy, Hash, PartialEq, Eq)]
-struct Rock {
-    pub class: usize,
-    pub pos: Pos,
-}
-
 pub fn main() {
     let s = include_bytes!("../input.txt");
     let jet_len = s.len() - 1;
@@ -148,57 +113,51 @@ pub fn main() {
     let mut map = Map::new();
 
     for i_rock in 0..2022 {
-        let mut rock = Rock {
-            class: i_rock % ROCKS.len(),
-            pos: Pos {
-                x: 2,
-                y: map.highest_rock + 3,
-            },
-        };
-        let mut last_pos = rock.pos;
+        let rock_i = i_rock % ROCKS.len();
+        let mut rock = ROCKS[rock_i];
+        let mut last_rock;
+        let mut rock_y = map.highest_rock + 3;
+        let mut last_y = rock_y;
         #[cfg(debug_assertions)]
         println!(
-            "{} ({}, {}) {}",
-            rock.class, rock.pos.x, rock.pos.y, map.highest_rock
+            "{} ({}) {}",
+            rock_i, rock_y, map.highest_rock
         );
-        while !map.collides_with(rock) {
+        while !map.collides_with(rock, rock_y) {
             #[cfg(debug_assertions)]
             println!(
-                "{} ({}, {}) {}",
-                rock.class, rock.pos.x, rock.pos.y, s[jet_i] as char
+                "{} ({}) {}",
+                rock_i, rock_y, s[jet_i] as char
             );
-            last_pos = rock.pos;
+            last_rock = rock;
             if s[jet_i] == b'<' {
-                rock.pos.x = rock.pos.x.wrapping_sub(1);
+                shift_rock_left(&mut rock);
             } else {
-                rock.pos.x = rock.pos.x.wrapping_add(1);
+                shift_rock_right(&mut rock);
             }
             jet_i = (jet_i + 1) % jet_len;
-            if map.collides_with(rock) {
-                rock.pos = last_pos;
+            if map.collides_with(rock, rock_y) {
+                rock = last_rock;
             }
             #[cfg(debug_assertions)]
-            println!("{} ({}, {}) v", rock.class, rock.pos.x, rock.pos.y);
-            last_pos = rock.pos;
-            rock.pos.y = rock.pos.y.wrapping_sub(1);
+            println!("{} ({}) v", rock_i, rock_y);
+            last_y = rock_y;
+            rock_y = rock_y.wrapping_sub(1);
         }
-        rock.pos = last_pos;
-        map.add_rock(rock);
+        rock_y = last_y;
+        map.add_rock(rock, rock_y);
         #[cfg(debug_assertions)]
         println!(
-            "{} ({}, {}) {}",
-            rock.class, rock.pos.x, rock.pos.y, map.highest_rock
+            "{} ({}) {}",
+            rock_i, rock_y, map.highest_rock
         );
-        // {
-        //     for y in 0..map.highest_rock {
-        //         let y = map.highest_rock - y - 1;
-        //         print!("|");
-        //         for x in 0..MAP_WIDTH {
-        //             print!("{}", map.contents[y][x] as char);
-        //         }
-        //         println!("|");
-        //     }
-        // }
+        #[cfg(debug_assertions)]
+        {
+            let highest_rock = map.highest_rock + 1;
+            for y in (highest_rock.saturating_sub(16)..=highest_rock).rev() {
+                println!("|{}| {}", format!("{:07b}", map.get_row(y)).replace("0", " "), y + 1);
+            }
+        }
     }
     print!("{} ", map.highest_rock);
 }
