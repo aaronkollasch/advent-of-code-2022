1634297774a8283b1fba36b9d82553daac2a3645
Aaron Kollasch <aaron@kollasch.dev>
2022-12-18 06:44:00 -0500

    Remove repeated parsing from day 18
    Speedup 629 µs / 1.67 ms -> 490 / 921 µs

--- day18a/src/main.rs
+++ day18a/src/main.rs
@@ -7,28 +7,28 @@ pub fn main() {
     // s.split(|b| *b == b'\n').for_each(|l| {
     // });
     let s = include_str!("../input.txt");
-    let cube_iter = s.lines().filter(|l| !l.is_empty()).map(|l| {
+    let cubes = s.lines().filter(|l| !l.is_empty()).map(|l| {
         let result: Vec<CubePos> = l
             .split(',')
             .map(|w| w.parse::<CubePos>().unwrap() * 2)
             .collect();
         (result[0], result[1], result[2])
-    });
-    let moved_cubes = cube_iter
-        .clone()
+    }).collect::<Vec<_>>();
+    let moved_cubes = cubes
+        .iter()
         .flat_map(|(a, b, c)| {
             [
                 (a + 1, b + 1, c + 2),
                 (a + 2, b + 1, c + 1),
                 (a + 1, b + 2, c + 1),
-                (a + 1, b + 1, c),
-                (a + 1, b, c + 1),
-                (a, b + 1, c + 1),
+                (a + 1, b + 1, *c),
+                (a + 1, *b, c + 1),
+                (*a, b + 1, c + 1),
             ]
             .into_iter()
         })
         .collect::<HashSet<_>>();
-    let num_cubes = cube_iter.count();
+    let num_cubes = cubes.len();
     let num_neighbors = num_cubes * 6 - moved_cubes.len();
     let num_surface = num_cubes * 6 - num_neighbors * 2;
 
--- day18b/src/main.rs
+++ day18b/src/main.rs
@@ -1,73 +1,7 @@
-use rustc_hash::{FxHashMap, FxHashSet};
-use std::collections::VecDeque;
+use rustc_hash::FxHashSet;
 
 type CubePos = i32;
 type Pos3d = (CubePos, CubePos, CubePos);
-
-pub fn is_internal(
-    cube: Pos3d,
-    cubes: &FxHashSet<Pos3d>,
-    cache: &mut FxHashMap<Pos3d, bool>,
-) -> bool {
-    let mut queue: VecDeque<(Pos3d, usize)> = VecDeque::with_capacity(1024);
-    queue.push_front((cube, 0));
-    let mut visited: FxHashSet<Pos3d> = Default::default();
-    visited.reserve(1024);
-    let mut result = None;
-    'outer: while !queue.is_empty() {
-        let (cube, depth) = queue.pop_front().unwrap();
-        // if depth > 20 { continue; }
-        if !cubes.contains(&cube) {
-            visited.insert(cube);
-        }
-        let (a, b, c) = cube;
-        if [
-            (a + 2, b, c),
-            (a, b + 2, c),
-            (a, b, c + 2),
-            (a - 2, b, c),
-            (a, b - 2, c),
-            (a, b, c - 2),
-        ]
-        .into_iter()
-        .all(|cube2| cubes.contains(&cube2) || *cache.get(&cube2).unwrap_or(&false))
-        {
-            result = Some(true);
-            break 'outer;
-        }
-        for cube2 in [
-            (a + 2, b, c),
-            (a, b + 2, c),
-            (a, b, c + 2),
-            (a - 2, b, c),
-            (a, b - 2, c),
-            (a, b, c - 2),
-        ]
-        .into_iter()
-        {
-            if let Some(val) = cache.get(&cube2) {
-                if *val {
-                    result = Some(false);
-                    break 'outer;
-                }
-            }
-            if !cubes.contains(&cube2) && !visited.contains(&cube2) {
-                visited.insert(cube2);
-                queue.push_back((cube2, depth + 1));
-            }
-        }
-    }
-    let result = if let Some(result) = result {
-        result
-    } else {
-        queue.is_empty()
-    };
-    for pos in visited.iter() {
-        cache.insert(*pos, result);
-    }
-
-    result
-}
 
 struct Map {
     pub contents: Vec<Vec<Vec<bool>>>,
@@ -121,43 +55,43 @@ pub fn main() {
     // s.split(|b| *b == b'\n').for_each(|l| {
     // });
     let s = include_str!("../input.txt");
-    let cube_iter = s.lines().filter(|l| !l.is_empty()).map(|l| {
+    let cubes = s.lines().filter(|l| !l.is_empty()).map(|l| {
         let result: Vec<CubePos> = l
             .split(',')
             .map(|w| w.parse::<CubePos>().unwrap() * 2)
             .collect();
         (result[0], result[1], result[2])
-    });
-    let a_pos = cube_iter
-        .clone()
+    }).collect::<Vec<_>>();
+    let a_pos = cubes
+        .iter()
         .map(|(a, _, _)| a)
         .collect::<FxHashSet<_>>();
     let (min_a, max_a) = (a_pos.iter().min().unwrap(), a_pos.iter().max().unwrap());
-    let b_pos = cube_iter
-        .clone()
+    let b_pos = cubes
+        .iter()
         .map(|(_, b, _)| b)
         .collect::<FxHashSet<_>>();
     let (min_b, max_b) = (b_pos.iter().min().unwrap(), b_pos.iter().max().unwrap());
-    let c_pos = cube_iter
-        .clone()
+    let c_pos = cubes
+        .iter()
         .map(|(_, _, c)| c)
         .collect::<FxHashSet<_>>();
     let (min_c, max_c) = (c_pos.iter().min().unwrap(), c_pos.iter().max().unwrap());
-    let sides = cube_iter
-        .clone()
+    let sides = cubes
+        .iter()
         .flat_map(|(a, b, c)| {
             [
-                (a + 1, b, c),
-                (a, b + 1, c),
-                (a, b, c + 1),
-                (a - 1, b, c),
-                (a, b - 1, c),
-                (a, b, c - 1),
+                (a + 1, *b, *c),
+                (*a, *b + 1, *c),
+                (*a, *b, c + 1),
+                (a - 1, *b, *c),
+                (*a, b - 1, *c),
+                (*a, *b, *c - 1),
             ]
             .into_iter()
         })
         .collect::<FxHashSet<_>>();
-    let num_cubes = cube_iter.clone().count();
+    let num_cubes = cubes.len();
     let num_neighbors = num_cubes * 6 - sides.len();
     let num_surface = num_cubes * 6 - num_neighbors * 2;
 
@@ -170,12 +104,12 @@ pub fn main() {
         println!("num surface sides: {}", num_surface);
     }
 
-    let cubes = cube_iter.clone().collect::<FxHashSet<_>>();
+    let cubes = cubes.iter().collect::<FxHashSet<_>>();
 
-    let cells: Vec<Vec<Vec<bool>>> = Vec::from_iter((min_c - 2..=max_c + 2).step_by(2).map(|c| {
-        Vec::from_iter((min_b - 2..=max_b + 2).step_by(2).map(|b| {
+    let cells: Vec<Vec<Vec<bool>>> = Vec::from_iter((*min_c - 2..=*max_c + 2).step_by(2).map(|c| {
+        Vec::from_iter((*min_b - 2..=*max_b + 2).step_by(2).map(|b| {
             Vec::from_iter(
-                (min_a - 2..=max_a + 2)
+                (*min_a - 2..=*max_a + 2)
                     .step_by(2)
                     .map(|a| cubes.contains(&(a, b, c))),
             )
@@ -234,7 +168,6 @@ pub fn main() {
                 .map(move |(a, _v)| (a as CubePos * 2, b as CubePos * 2, c as CubePos * 2))
         })
     });
-    let contained_cube_count = contained_cube_iter.clone().count();
 
     let sides2 = contained_cube_iter
         .clone()
@@ -256,7 +189,6 @@ pub fn main() {
 
     #[cfg(debug_assertions)]
     {
-        println!("contained cubes: {}", contained_cube_count);
         println!("num cubes2: {}", num_cubes2);
         println!("num neighbors2: {}", num_neighbors2);
         println!("num surface sides2: {}", num_surface2);
