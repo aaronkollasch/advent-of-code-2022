229557a02ac4661e7edc43c3aa507c65e930515b
Aaron Kollasch <aaron@kollasch.dev>
2022-12-18 07:19:50 -0500

    Convert day 18a to FxHashSet, 327 µs

--- day18a/src/main.rs
+++ day18a/src/main.rs
@@ -1,33 +1,37 @@
-use std::collections::HashSet;
+use rustc_hash::FxHashSet;
 
-type CubePos = u8;
+type CubePos = i8;
 
 pub fn main() {
     // let s = include_bytes!("../input.txt");
     // s.split(|b| *b == b'\n').for_each(|l| {
     // });
     let s = include_str!("../input.txt");
-    let cubes = s.lines().filter(|l| !l.is_empty()).map(|l| {
-        let result: Vec<CubePos> = l
-            .split(',')
-            .map(|w| w.parse::<CubePos>().unwrap() * 2)
-            .collect();
-        (result[0], result[1], result[2])
-    }).collect::<Vec<_>>();
+    let cubes = s
+        .lines()
+        .filter(|l| !l.is_empty())
+        .map(|l| {
+            let result: Vec<CubePos> = l
+                .split(',')
+                .map(|w| w.parse::<CubePos>().unwrap() * 2)
+                .collect();
+            (result[0], result[1], result[2])
+        })
+        .collect::<Vec<_>>();
     let moved_cubes = cubes
         .iter()
         .flat_map(|(a, b, c)| {
             [
-                (a + 1, b + 1, c + 2),
-                (a + 2, b + 1, c + 1),
-                (a + 1, b + 2, c + 1),
-                (a + 1, b + 1, *c),
-                (a + 1, *b, c + 1),
-                (*a, b + 1, c + 1),
+                (a + 1, *b, *c),
+                (*a, b + 1, *c),
+                (*a, *b, c + 1),
+                (a - 1, *b, *c),
+                (*a, b - 1, *c),
+                (*a, *b, c - 1),
             ]
             .into_iter()
         })
-        .collect::<HashSet<_>>();
+        .collect::<FxHashSet<_>>();
     let num_cubes = cubes.len();
     let num_neighbors = num_cubes * 6 - moved_cubes.len();
     let num_surface = num_cubes * 6 - num_neighbors * 2;
