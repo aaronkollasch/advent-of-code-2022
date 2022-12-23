e099614590b23fa3956050986d0a8b028cd06177
Aaron Kollasch <aaron@kollasch.dev>
2022-12-23 13:23:50 -0500

    Do cargo fmt day 23

--- day23a/src/main.rs
+++ day23a/src/main.rs
@@ -1,15 +1,20 @@
-use rustc_hash::FxHashSet as HashSet;
-use std::cmp::{min, max};
 #[cfg(debug_assertions)]
 use kdam::{tqdm, BarExt};
+use rustc_hash::FxHashSet as HashSet;
+use std::cmp::{max, min};
 
 type Number = isize;
 type Pos = (Number, Number);
 
 const SURROUND: [Pos; 8] = [
-    (1, 1), (1, 0), (1, -1),
-    (0, 1), (0, -1),
-    (-1, 1), (-1, 0), (-1, -1),
+    (1, 1),
+    (1, 0),
+    (1, -1),
+    (0, 1),
+    (0, -1),
+    (-1, 1),
+    (-1, 0),
+    (-1, -1),
 ];
 
 const DIRECTIONS: [[Pos; 3]; 4] = [
@@ -22,7 +27,8 @@ const DIRECTIONS: [[Pos; 3]; 4] = [
 #[cfg(debug_assertions)]
 fn print_elves(elves: &HashSet<Pos>) {
     println!("{:?}", elves);
-    let (mut min_x, mut max_x, mut min_y, mut max_y) = (isize::MAX, isize::MIN, isize::MAX, isize::MIN);
+    let (mut min_x, mut max_x, mut min_y, mut max_y) =
+        (isize::MAX, isize::MIN, isize::MAX, isize::MIN);
     for elf in elves.iter() {
         min_x = min(min_x, elf.0);
         max_x = max(max_x, elf.0);
@@ -42,10 +48,16 @@ fn print_elves(elves: &HashSet<Pos>) {
 }
 
 fn dest(elf: Pos, t: usize, elves: &HashSet<Pos>) -> Pos {
-    if SURROUND.iter().any(|d| elves.contains(&(elf.0 + d.0, elf.1 + d.1))) {
+    if SURROUND
+        .iter()
+        .any(|d| elves.contains(&(elf.0 + d.0, elf.1 + d.1)))
+    {
         for dir in t..t + 4 {
             let dirs = DIRECTIONS[dir % 4];
-            if !dirs.iter().any(|d| elves.contains(&(elf.0 + d.0, elf.1 + d.1))) {
+            if !dirs
+                .iter()
+                .any(|d| elves.contains(&(elf.0 + d.0, elf.1 + d.1)))
+            {
                 let dir = dirs[1];
                 let p = (elf.0 + dir.0, elf.1 + dir.1);
                 return p;
@@ -94,7 +106,8 @@ pub fn main() {
         #[cfg(debug_assertions)]
         pb.update(1);
     }
-    let (mut min_x, mut max_x, mut min_y, mut max_y) = (isize::MAX, isize::MIN, isize::MAX, isize::MIN);
+    let (mut min_x, mut max_x, mut min_y, mut max_y) =
+        (isize::MAX, isize::MIN, isize::MAX, isize::MIN);
     for elf in elves.iter() {
         min_x = min(min_x, elf.0);
         max_x = max(max_x, elf.0);
--- day23b/src/main.rs
+++ day23b/src/main.rs
@@ -1,16 +1,21 @@
-use rustc_hash::FxHashSet as HashSet;
-#[cfg(debug_assertions)]
-use std::cmp::{min, max};
 #[cfg(debug_assertions)]
 use kdam::{tqdm, BarExt};
+use rustc_hash::FxHashSet as HashSet;
+#[cfg(debug_assertions)]
+use std::cmp::{max, min};
 
 type Number = isize;
 type Pos = (Number, Number);
 
 const SURROUND: [Pos; 8] = [
-    (1, 1), (1, 0), (1, -1),
-    (0, 1), (0, -1),
-    (-1, 1), (-1, 0), (-1, -1),
+    (1, 1),
+    (1, 0),
+    (1, -1),
+    (0, 1),
+    (0, -1),
+    (-1, 1),
+    (-1, 0),
+    (-1, -1),
 ];
 
 const DIRECTIONS: [[Pos; 3]; 4] = [
@@ -23,7 +28,8 @@ const DIRECTIONS: [[Pos; 3]; 4] = [
 #[cfg(debug_assertions)]
 fn print_elves(elves: &HashSet<Pos>) {
     println!("{:?}", elves);
-    let (mut min_x, mut max_x, mut min_y, mut max_y) = (isize::MAX, isize::MIN, isize::MAX, isize::MIN);
+    let (mut min_x, mut max_x, mut min_y, mut max_y) =
+        (isize::MAX, isize::MIN, isize::MAX, isize::MIN);
     for elf in elves.iter() {
         min_x = min(min_x, elf.0);
         max_x = max(max_x, elf.0);
@@ -43,10 +49,16 @@ fn print_elves(elves: &HashSet<Pos>) {
 }
 
 fn dest(elf: Pos, t: usize, elves: &HashSet<Pos>) -> Pos {
-    if SURROUND.iter().any(|d| elves.contains(&(elf.0 + d.0, elf.1 + d.1))) {
+    if SURROUND
+        .iter()
+        .any(|d| elves.contains(&(elf.0 + d.0, elf.1 + d.1)))
+    {
         for dir in t..t + 4 {
             let dirs = DIRECTIONS[dir % 4];
-            if !dirs.iter().any(|d| elves.contains(&(elf.0 + d.0, elf.1 + d.1))) {
+            if !dirs
+                .iter()
+                .any(|d| elves.contains(&(elf.0 + d.0, elf.1 + d.1)))
+            {
                 let dir = dirs[1];
                 let p = (elf.0 + dir.0, elf.1 + dir.1);
                 return p;
