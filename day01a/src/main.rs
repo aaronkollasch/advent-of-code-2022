c34a571ca6e8844e2ca60ef424735c80d2dfeade
Aaron Kollasch <aaron@kollasch.dev>
2022-12-25 12:59:18 -0500

    Speedup day 1, 31 / 32 µs -> 10 / 11 µs

--- day01a/src/main.rs
+++ day01a/src/main.rs
@@ -1,10 +1,17 @@
+use std::cmp::max;
+
 pub fn main() {
-    print!(
-        "{} ",
-        include_str!("../input.txt")
-            .split("\n\n")
-            .map(|elf| elf.lines().map(|c| c.parse::<u32>().unwrap_or(0)).sum::<u32>())
-            .max()
-            .unwrap_or(0)
-    );
+    let mut elf_total = 0u32;
+    let mut max_elf = 0u32;
+    include_bytes!("../input.txt")
+        .split(|b| *b == b'\n')
+        .for_each(|l| {
+            if l.is_empty() {
+                max_elf = max(max_elf, elf_total);
+                elf_total = 0;
+            } else {
+                elf_total += l.iter().fold(0, |acc, x| acc * 10 + (x - b'0') as u32);
+            }
+        });
+    print!("{} ", max_elf);
 }
--- day01b/src/main.rs
+++ day01b/src/main.rs
@@ -3,18 +3,22 @@ use std::collections::BinaryHeap;
 
 pub fn main() {
     let num_top = 3;
-    let mut top_elves: BinaryHeap<Reverse<u32>> = BinaryHeap::with_capacity(num_top+1);
+    let mut top_elves: BinaryHeap<Reverse<u32>> = BinaryHeap::with_capacity(num_top + 1);
     for _ in 0..num_top {
         top_elves.push(Reverse::<u32>(0));
     }
 
-    include_str!("../input.txt").split("\n\n").for_each(|elf| {
-        top_elves.push(Reverse::<u32>(
-            elf.lines()
-                .map(|c| c.parse::<u32>().unwrap_or(0))
-                .sum::<u32>(),
-        ));
-        top_elves.pop();
-    });
+    let mut elf_total = 0u32;
+    include_bytes!("../input.txt")
+        .split(|b| *b == b'\n')
+        .for_each(|l| {
+            if l.is_empty() {
+                top_elves.push(Reverse::<u32>(elf_total));
+                top_elves.pop();
+                elf_total = 0;
+            } else {
+                elf_total += l.iter().fold(0, |acc, x| acc * 10 + (x - b'0') as u32);
+            }
+        });
     print!("{} ", top_elves.iter().map(|e| e.0).sum::<u32>());
 }
