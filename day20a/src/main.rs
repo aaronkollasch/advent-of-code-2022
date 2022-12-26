af2571e3cd1af62e126409db1b7266559ab00358
Aaron Kollasch <aaron@kollasch.dev>
2022-12-26 16:31:36 -0500

    Do cargo fmt day 20

--- day20a/src/main.rs
+++ day20a/src/main.rs
@@ -1,14 +1,16 @@
-use std::collections::VecDeque;
 use common::parse_signed;
+use std::collections::VecDeque;
 
 type Number = isize;
 
 pub fn main() {
     let s = include_bytes!("../input.txt");
     let mut numbers: VecDeque<(Number, bool)> = VecDeque::with_capacity(8192);
-    s.split(|b| *b == b'\n').filter(|l| !l.is_empty()).for_each(|l| {
-        numbers.push_back((parse_signed::<Number>(l), false));
-    });
+    numbers.extend(
+        s.split(|b| *b == b'\n')
+            .filter(|l| !l.is_empty())
+            .map(|l| (parse_signed::<Number>(l), false)),
+    );
     let mut num_moves = 0;
     let mut index = 0;
     let len = numbers.len();
@@ -17,7 +19,10 @@ pub fn main() {
             index = (index + 1) % len;
         }
         #[cfg(debug_assertions)]
-        println!("{:?} {index}", numbers.iter().map(|t| t.0).collect::<Vec<_>>());
+        println!(
+            "{:?} {index}",
+            numbers.iter().map(|t| t.0).collect::<Vec<_>>()
+        );
         let (amount, _) = numbers[index];
         let new_index = (index as Number + amount).rem_euclid((len - 1) as Number) as usize;
         #[cfg(debug_assertions)]
@@ -28,9 +33,15 @@ pub fn main() {
     }
     let zpos = numbers.iter().position(|(n, _i)| *n == 0).unwrap();
     #[cfg(debug_assertions)]
-    println!("{} {} {}", numbers[(zpos + 1000) % len].0, numbers[(zpos + 2000) % len].0, numbers[(zpos + 3000) % len].0);
-    let result = [
-        zpos + 1000, zpos + 2000, zpos + 3000
-    ].iter().map(|n| numbers[n % len].0).sum::<Number>();
+    println!(
+        "{} {} {}",
+        numbers[(zpos + 1000) % len].0,
+        numbers[(zpos + 2000) % len].0,
+        numbers[(zpos + 3000) % len].0
+    );
+    let result = [zpos + 1000, zpos + 2000, zpos + 3000]
+        .iter()
+        .map(|n| numbers[n % len].0)
+        .sum::<Number>();
     print!("{} ", result);
 }
--- day20b/src/main.rs
+++ day20b/src/main.rs
@@ -1,23 +1,29 @@
 use std::collections::VecDeque;
+use common::parse_signed;
 // TODO: see https://www.reddit.com/r/adventofcode/comments/zqezkn/comment/j0yaksg
 // TODO: see https://github.com/p88h/aoc2022/blob/main/lib/day20.cs
 // TODO: see https://github.com/orlp/aoc2022/blob/master/src/treap.rs
-use common::parse_signed;
 
 type Number = isize;
 
 pub fn main() {
     let s = include_bytes!("../input.txt");
     let mut numbers: VecDeque<(Number, usize)> = VecDeque::with_capacity(32);
-    s.split(|b| *b == b'\n').filter(|l| !l.is_empty()).enumerate().for_each(|(i, l)| {
-        numbers.push_back((parse_signed::<Number>(l) * 811589153, i));
-    });
+    numbers.extend(
+        s.split(|b| *b == b'\n')
+            .filter(|l| !l.is_empty())
+            .enumerate()
+            .map(|(i, l)| (parse_signed::<Number>(l) * 811589153, i)),
+    );
     let len = numbers.len();
     for _round in 0..10 {
         for idx in 0..len {
             let index = numbers.iter().position(|(_n, i)| *i == idx).unwrap();
             #[cfg(debug_assertions)]
-            println!("{:?} {index}", numbers.iter().map(|t| t.0).collect::<Vec<_>>());
+            println!(
+                "{:?} {index}",
+                numbers.iter().map(|t| t.0).collect::<Vec<_>>()
+            );
             let (amount, _) = numbers[index];
             let new_index = index as Number + amount;
             let new_index = new_index.rem_euclid((len - 1) as Number) as usize;
@@ -29,9 +35,15 @@ pub fn main() {
     }
     let zpos = numbers.iter().position(|(n, _i)| *n == 0).unwrap();
     #[cfg(debug_assertions)]
-    println!("{} {} {}", numbers[(zpos + 1000) % len].0, numbers[(zpos + 2000) % len].0, numbers[(zpos + 3000) % len].0);
-    let result = [
-        zpos + 1000, zpos + 2000, zpos + 3000
-    ].iter().map(|n| numbers[n % len].0).sum::<Number>();
+    println!(
+        "{} {} {}",
+        numbers[(zpos + 1000) % len].0,
+        numbers[(zpos + 2000) % len].0,
+        numbers[(zpos + 3000) % len].0
+    );
+    let result = [zpos + 1000, zpos + 2000, zpos + 3000]
+        .iter()
+        .map(|n| numbers[n % len].0)
+        .sum::<Number>();
     print!("{} ", result);
 }
